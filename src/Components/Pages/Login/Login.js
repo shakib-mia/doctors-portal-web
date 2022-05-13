0dac943fd3d49820679a410b4be645e9078d4d9f
shakib-mia <smdshakibmia2001@gmail.com>
2022-05-13 20:56:15 +0600

    login functionality and animations added

--- src/Components/Pages/Login/Login.js
+++ src/Components/Pages/Login/Login.js
@@ -1,25 +1,35 @@
 import React from 'react';
-import { useSignInWithGoogle } from "react-firebase-hooks/auth"
+import { useSignInWithEmailAndPassword, useSignInWithGoogle } from "react-firebase-hooks/auth"
 import auth from '../../../firebase.init';
 import { useForm } from "react-hook-form";
+import Loading from '../../Shared/Loading';
 
 const Login = () => {
-      const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+      const [signInWithGoogle, googleUser, googleLoading, googleError] = useSignInWithGoogle(auth);
+      const [signInWithEmailAndPassword, user, loading, error] = useSignInWithEmailAndPassword(auth);
+
+      let signInErrorMessage;
+
       const { register, formState: { errors }, handleSubmit } = useForm();
       const onSubmit = data => {
-            console.log(data)
+            console.log(data);
+            signInWithEmailAndPassword(data.email, data.password)
       };
 
-      if (user) {
-            console.log(user);
+      if (loading || googleLoading) {
+            return <Loading />
       }
 
-      if (loading) {
-            console.log("Loading...")
+      if (googleUser || user) {
+            console.log(googleUser || user);
       }
 
-      if (error) {
-            console.log(error)
+      if (error || googleError) {
+            signInErrorMessage = <p className='text-red-600'>{error?.message}</p>
+      }
+
+      if (googleError) {
+            console.log(googleError)
       }
 
       return (
@@ -78,7 +88,7 @@ const Login = () => {
 
                                           </label>
                                     </div>
-
+                                    {signInErrorMessage}
                                     <input className='btn w-full max-w-xs' type="submit" value="Login" />
                               </form>
                               <div className="divider">OR</div>
